ee467fb40c600acf38b46814a584424d7fb74f4d
Sulfion <2246364@swansea.ac.uk>
2022-11-24 18:02:46 +0000

    created timer for raycasts

--- Assets/Scripts/SteamController.cs
+++ Assets/Scripts/SteamController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SteamController : MonoBehaviour
 {
     public Vector3 collision = Vector3.zero;
+    private float timer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +16,27 @@ public class SteamController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        DistanceFromSteamCheckAndRaycast();
+    }
+
+    //create raycast, check if gameobject is player using tag, if it is a player, play steam particle effect
+    //check how long player was in raycast, if 3seconds or longer play smoke particle
+    public void DistanceFromSteamCheckAndRaycast()
+    {
         var ray = new Ray(this.transform.position, this.transform.right);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, maxDistance:3.5f))
+        if (Physics.Raycast(ray, out hit, maxDistance: 3.5f))
         {
             if (hit.collider.CompareTag("Player"))
             {
-                var part = GetComponent<ParticleSystem>();
-                part.Play();
-                Debug.Log("Hit");
+                timer += Time.deltaTime;
+                if (timer >= 3)
+                {
+                    var part = GetComponent<ParticleSystem>();
+                    part.Play();
+                    Debug.Log("Hit");
+                    timer = 0;
+                }
             }
         }
     }
